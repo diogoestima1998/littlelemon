63239a0d01e0fb88372b0b4b175bfc9c57cf355d
Diogo <d.estima@campus.fct.unl.pt>
2023-06-12 22:33:08 +0100

    Structure commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"; 
+import Hero from "./components/Hero"; 
+import Highlights from "./components/Highlights"; 
+import Testimonials from "./components/Testimonials"; 
+import About from "./components/About"; 
+import Footer from "./components/Footer"; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          This is my world. yup i dont give a FFFFFF. BUT I DONT CARE.
-        </h1>
-      </header>
-    </div>
+    <main>
+      <Header/>
+      <Hero/>
+      <Highlights/>
+      <Testimonials/>
+      <About/>
+      <Footer/>
+    </main>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <header>
+        <meta name="description" content="Little Lemon: Savor the Best of Mediterranean Cuisine with a Modern Twist at Our Family-Owned Restaurant!"/>
+        <meta name="og:title" content="Little Lemon"/>
+        <meta name="og:description" content="Little Lemon: Savor the Best of Mediterranean Cuisine with a Modern Twist at Our Family-Owned Restaurant!"/>
+        <meta name="og:image" content="../images/Logo.svg"/>
+        <div>
+            <img src='./src/images/Logo.svg' alt='logo' width="300px"/>
+           
+            Header</div>
+    </header>
+    
+  )
+}
+
+export default Header
